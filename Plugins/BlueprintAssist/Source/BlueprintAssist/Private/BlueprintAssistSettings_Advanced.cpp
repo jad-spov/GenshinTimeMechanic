214f8824dec6dceeba2ab45e723bff0a9db89703
Jad-spov <jad-ext@spov.tv>
2023-07-15 20:38:28 +0100

    added blueprint assist plugin

---
+++ Plugins/BlueprintAssist/Source/BlueprintAssist/Private/BlueprintAssistSettings_Advanced.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BlueprintAssistSettings_Advanced.h"
+
+UBASettings_Advanced::UBASettings_Advanced(const FObjectInitializer& ObjectInitializer)
+{
+	bGenerateUniqueGUIDForMaterialExpressions = false;
+	bStoreCacheDataInPackageMetaData = false;
+}
---
+++ Plugins/BlueprintAssist/Source/BlueprintAssist/Public/BlueprintAssistSettings_Advanced.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "BlueprintAssistSettings_Advanced.generated.h"
+
+UCLASS(config = EditorPerProjectUserSettings)
+class BLUEPRINTASSIST_API UBASettings_Advanced final : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UBASettings_Advanced(const FObjectInitializer& ObjectInitializer);
+
+	/* Fix for issue where copy-pasting material nodes will result in their material expressions having the same GUID */
+	UPROPERTY(EditAnywhere, config, Category = Experimental)
+	bool bGenerateUniqueGUIDForMaterialExpressions;
+
+	UPROPERTY(EditAnywhere, config, Category = Experimental)
+	bool bStoreCacheDataInPackageMetaData;
+
+	UPROPERTY(EditAnywhere, config, Category = Default)
+	TSet<FName> DisabledCommands;
+};
